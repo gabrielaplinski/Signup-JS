c4fd57b4f02b5894d69386fcfca60f044f98f1d9
Gabriela Piovesan Plinski <gabrielaplinski@outlook.com>
2025-11-07 15:35:40 -0300

    styles/js update

--- script/cad.js
+++ script/cad.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('formulario');
 const inputs = form.querySelectorAll('input:not([type="submit"])');
 const messages = form.querySelectorAll('.msg');
+const signin = document.querySelector('#signin');
+const price = document.querySelector('#price');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -8,36 +10,54 @@ form.addEventListener('submit', (e) => {
 
     inputs.forEach((input, index) => {
         const value = input.value.trim();
+        const message = messages[index];
+        let errorMsg = '';
 
         if (value === '') {
+            errorMsg = `${input.placeholder} cannot be empty`;
+        }
+        else if (input.type === 'email' && !/\S+@\S+\.\S+/.test(value)) {
+            errorMsg = "Looks like this is not an email";
+        }
+        if (errorMsg) {
             input.classList.add('error');
             input.classList.remove('success');
-            messages[index].textContent = `${input.placeholder} cannot be empty`;
+            message.textContent = errorMsg;
             valid = false;
         }
-        else if (input.type === 'email') {
-            const emailValido = /\S+@\S+\.\S+/.test(value);
-            if (!emailValido) {
-                input.classList.add('error');
-                input.classList.remove('success');
-                messages[index].textContent = "Looks like this is not an email"
-                valid = false;
-            } else {
-                input.classList.add('success');
-                input.classList.remove('error');
-                messages[index].textContent = '';
-            }
-        } 
         else {
             input.classList.add('success');
             input.classList.remove('error');
-            messages[index].textContent = '';
+            messages.textContent = '';
         }
     });
 
     if (valid) {
-        alert('form submitted');
+        removerErro();
+        alert('Form submitted');
         form.reset();
         inputs.forEach(input => input.classList.remove('success'));
+    } 
+    else {
+        mostrarErro();
     }
+});
+
+function mostrarErro() {
+    signin.classList.add('error-active');
+    price.classList.add('error-active');
+}
+
+function removerErro() {
+    signin.classList.remove('error-active');
+    price.classList.remove('error-active');
+}
+
+inputs.forEach((input,index) => {
+    input.addEventListener('input', () => {
+        if (input.value.trim() !== '') {
+            input.classList.remove('error');
+            messages[index].textContent = '';
+        }
+    });
 });
